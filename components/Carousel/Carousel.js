84d9384e6aee0bbaaf8666c6c79e5ee0e70ca0d7
Leana <leananeparidze.com>
2019-06-22 00:06:38 -0700

    implements basic carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,44 +1,49 @@
+let state = 1;
+console.log(state, "STATE")
 class Carousel {
   constructor(element) {
     this.element = element;
-    this.left = document.querySelector('.left-button');
-    // console.log(this.left)
-    this.right = document.querySelector('.right-button');
-    // console.log(this.right)
-
-    this.left.addEventListener('click', () => this.handleLeft());
-    this.right.addEventListener('click', () => this.handleRight());
+    this.element.addEventListener('click', () => this.handleClick(event))
+  }
+  handleClick() {
+    if (this.element.className === 'left-button') {
+      this.handleLeft(event);
+    } else {
+      this.handleRight(event);
+    }
   }
   handleLeft() {
-
+    if (state === 1) {
+      document.querySelector(`.img[data-num="${state}"]`).classList.remove('selected');
+      state = 4;
+      document.querySelector(`.img[data-num="${state}"]`).classList.add('selected')
+    } else {
+      document.querySelector(`.img[data-num="${state}"]`).classList.remove('selected');
+      --state;
+      document.querySelector(`.img[data-num="${state}"]`).classList.add('selected')
+    }
   }
-  handleRight() {
-    this.element.classList.add('slide-left');
-    // this.element.classList.remove('selected')
-    // document.querySelector('.carousel').children[2].classList.add('slide')
-    // console.log('click')
+  handleRight(event) {
+    event.preventDefault()
+    if (state === 4) {
+      document.querySelector(`.img[data-num="${state}"]`).classList.remove('selected');
+      state = 1;
+      document.querySelector(`.img[data-num="${state}"]`).classList.add('selected');
+    } else {
+      document.querySelector(`.img[data-num="${state}"]`).classList.remove('selected');
+      ++state;
+      document.querySelector(`.img[data-num="${state}"]`).classList.add('selected');
+    }
   }
 }
-// var margin = 200;
-let carousel = document.querySelector('.carousel');
-Array.from(carousel.children).forEach((item, index) => {
-  
-  if (index !== 0 || index !== carousel.children.length - 1) {
-    item.setAttribute('data-num', index)
-  }
-  // if (index === 1) {
-  //   item.setAttribute('style', 'margin-left:0%')
-  //   console.log(item)
-  // }
-  // if (index !== 1 && index !== 0 && index !== carousel.children.length - 1) {
-    
-  //   // let str = String(format: "%.2f%%", rate)
-  //   item.setAttribute('style', `margin-left:${margin}%`);
-  //   margin += 300;
-  //   console.log(margin, item)
-  // }
-  new Carousel(item)
-})
+
+new Carousel(document.querySelector('.left-button'));
+new Carousel(document.querySelector('.right-button'));
+
+// console.log(carousel)
+// Array.from(carousel.children).forEach(item => {
+//   new Carousel(item)
+// })
 
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
