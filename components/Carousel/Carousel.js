5ab2b39222d5943099e42a1080367a89448de18e
Leana <leananeparidze.com>
2019-06-22 18:09:51 -0700

    adds opacity transition to carousel images

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,7 +17,7 @@ class Carousel {
     } else {
       this.element.querySelector(`.img[data-num="${this.state}"]`).classList.remove('selected');
       --this.state;
-      this.element.querySelector(`.img[data-num="${this.state}"]`).classList.add('selected')
+      this.element.querySelector(`.img[data-num="${this.state}"]`).classList.add('selected');
     }
   }
   handleRight(event) {
@@ -35,7 +35,7 @@ class Carousel {
 }
 
 let carousel = document.querySelector('.carousel');
-console.log(new Carousel(carousel))
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
